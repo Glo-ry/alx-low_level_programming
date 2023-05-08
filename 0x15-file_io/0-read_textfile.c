d9acc9b43e4a4d8d75de0ff83c144b3ef01af66e
Glory Ochiobi <gloryochiobi0@gmail.com>
2023-05-08 16:15:29 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,33 +10,19 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	char *buffer;
+	ssize_t file_descriptor;
+	ssize_t bytes_written;
+	ssize_t bytes_read;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
 		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	bytes_read = read(file_descriptor, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
-	char *buf = malloc(sizeof(char) * letters);
-
-	if (!buf)
-	{
-		close(fd);
-		return (0);
-	}
-
-	t = read(fd, buf, letters);
-	if (t == -1)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
-
-	w = write(STDOUT_FILENO, buf, t);
-
-	free(buf);
-	close(fd);
-	return (w);
+	free(buffer);
+	close(file_descriptor);
+	return (bytes_written);
 }
