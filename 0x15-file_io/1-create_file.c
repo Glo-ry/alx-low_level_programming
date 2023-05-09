597233ecb2125205e8d655ae35443dffa912de2c
Glory Ochiobi <gloryochiobi0@gmail.com>
2023-05-08 17:36:55 -0700

    Creat file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,33 +7,34 @@
  *
  * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor, bytes_written, length = 0;
+	int file_descriptor, write_result, length = 0;
+	int result = 1;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		while (text_content[length] != '\0')
+		char *text_ptr = text_content;
+
+		while (*text_ptr++)
 			length++;
 	}
 
-	mode_t file_permissions = S_IRUSR | S_IWUSR;
-
-	file_descriptor = open(filename,
-			O_CREAT | O_WRONLY | O_TRUNC, file_permissions);
+	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file_descriptor == -1)
 		return (-1);
 
-	bytes_written = write(file_descriptor, text_content, length);
-	if (bytes_written == -1)
-	{
-		close(file_descriptor);
-		return (-1);
-	}
+	write_result = write(file_descriptor, text_content, length);
+	if (write_result == -1)
+		result = -1;
 
-	close(file_descriptor);
-	return (1);
+	if (close(file_descriptor) == -1)
+		result = -1;
+
+	return (result);
 }
+
